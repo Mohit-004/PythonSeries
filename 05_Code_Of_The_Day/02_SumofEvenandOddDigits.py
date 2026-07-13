1d85ffc0daca31f02b9c4ff75d442303216601bc
mohit <mohitghegadmal2004@gmail.com>
2026-07-13 21:15:32 +0530

    Code of the Day - Question 2

---
+++ 05_Code_Of_The_Day/02_SumofEvenandOddDigits.py
@@ -0,0 +1,20 @@
+number = int(input('Enter the number : '))
+
+evenSum = 0
+oddSum = 0
+
+temp_n = number
+
+while temp_n > 0:
+    digit = temp_n % 10
+
+    if digit % 2 == 0 :
+        evenSum += digit
+    
+    else :
+        oddSum += digit
+
+    temp_n = temp_n // 10
+
+print(f"Even sum : {evenSum}")
+print(f"Odd Sum : {oddSum}")
