ad0b76384ddf28d35832321fe426cfe225e22e7d
mohit <mohitghegadmal2004@gmail.com>
2026-07-21 23:26:28 +0530

    Project - Hotel Management System

---
+++ 01-Python_Introduction/Projects/15_HotelManagementSystem.py
@@ -0,0 +1,22 @@
+rooms = {
+    "101": {
+        "type": "Single",
+        "price": 1200,
+        "available": True
+    },
+    "102": {
+        "type": "Single",
+        "price": 1200,
+        "available": True
+    },
+    "201": {
+        "type": "Double",
+        "price": 2000,
+        "available": True
+    },
+    "301": {
+        "type": "Deluxe",
+        "price": 3500,
+        "available": True
+    }
+}
